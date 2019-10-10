37b8f75a233eca6a67ea95a9fdc7f7cf7cf5fe04
Daniel Moesgaard <moesgaarda@gmail.com>
2019-10-10 21:10:17 +0200

    Added more functions

--- main.rkt
+++ main.rkt
@@ -1,35 +1,7 @@
- #lang racket
-(require racket/pretty)
+#lang racket
 
 ;; Load the students file and convert it to a list
 (define student-list (car (file->list "all-students.lsp")))
-
-;; Helper method to find every nth student
-(define (every-nth-student lst n)
-  (if (null? lst)
-      '()
-      (cons (car lst) (every-nth-student (my-list-tail lst n) n))
-   )
-)
-
-
-;; Own implementation of list-tail that does not throw an error if index is
-;; larger than elements left.
-(define (my-list-tail list amount)
-  (if (or (= amount 0) (not (pair? list)))
-        list
-       (my-list-tail (cdr list) (- amount 1))
-  )
-)
-
-;; Helper method to return the first n students of a list
-(define (list-prefix lst n)
-  (if (= n 0)
-      '()
-      (cons (car lst) (list-prefix (cdr lst) (- n 1)))
-  )
-)
-
 
 ;; Function that returns a given group from a grouping
 (define (get-group-members sl groupNumber)
@@ -43,12 +15,41 @@
 )
 
 ;; Function that returns the number of groups in a grouping
+(define (groups-in-grouping sl)
+  (length (unique-groups sl)))
+
+;; Helper method to find unique group numbers
+(define (unique-groups sl)
+  (if (null? sl)
+      '()
+      (cons (car sl)
+            ;; Go through elements and filter all other groups
+            ;; with the same ID.
+            (unique-groups
+             (filter
+              (lambda (x) (not (equal? (car x) (caar sl)))) 
+             (cdr sl))))
+  )
+)
 
 ;; Function that returns the maximum group size in a grouping
+(define (maximum-group-size sl)
+  (group-size-finder 1 sl > -inf.0))
+
+;; Helper method that compares each group size based on cmp function input
+(define (group-size-finder currentGroupId sl cmp currentBest)
+  (if (null? (get-group-members sl currentGroupId))
+      currentBest
+      (if (cmp (length (get-group-members sl currentGroupId)) currentBest)
+          (group-size-finder (+ currentGroupId 1) sl cmp (length (get-group-members sl currentGroupId)))
+          (group-size-finder (+ currentGroupId 1) sl cmp currentBest)
+      )
+  )
+)
 
 ;; Function that returns the minimum group size in a grouping
-
-
+(define (minimum-group-size sl)
+  (group-size-finder 1 sl < +inf.0))
 
 ;; Constructor function for a single student
 
@@ -67,55 +68,70 @@
 ;; Predicate that identifies a grouping object
 
 ;; Pretty printer for students
-
-;; Pretty printer for groups
-(define (pp-group groupList)
-  (for ([i groupList])
-    (pretty-print (caddr i)))
- )
-  
-
-;; Pretty printer for groupings
-
-;; Random grouping
-(define (group-random sl gsl)
-    (random-group-constructor (shuffle sl) gsl 1))
-
-;; Helper method to randomly group students
-(define (random-group-constructor sl gsl count)
-  (if (null? sl)
-      '()
-  (if (>= (length sl) gsl)
-      (cons (cons count (list-prefix sl gsl)) (random-group-constructor (list-tail sl gsl) gsl (+ 1 count)))
-      (cons (cons count (list-prefix sl (length sl))) (random-group-constructor (list-tail '() 0) gsl 1))
-      )
+(define (print-student student)
+  (let ([group (car student)]
+        [id (cadr student)]
+        [name (caddr student)]
+        [sex  (cadddr student)]
+        [nationality (car (cddddr (stud)))])
+    (printf "Group: ~a\nId: ~s\nName: ~v\nSex: ~v\nNationality: ~v\n\n"
+            group id name sex nationality)
   )
 )
 
-(define rg (group-random student-list 5))
+;; Pretty printer for groups
+(define (print-group gl)
+  (for-each (lambda (student)
+         (print-student student))
+         gl)
+)
+
+;; Pretty printer for groupings
+(define (print-grouping gl)
+  (for-each (lambda (student)
+         (print-student student))
+         gl))
+
+;; Random grouping
+
 
 ;; Grouping by counting
 ; sl is a list of students
-; gsl is the desired group size
-(define (group-counting sl gsl)
-    (count-group-constructor sl gsl 1)
+; gsl is the desired amount of groups
+(define (group-counting sl ga)
+    (count-group-constructor sl ga 1)
 )
 
-(define (count-group-constructor sl gsl count)
+(define (count-group-constructor sl ga count)
   (if (null? sl)
       '()
-  (if (< count gsl)
-      (cons (cons count (car sl)) (count-group-constructor (cdr sl) gsl (+ count 1)))
-      (cons (cons count (car sl)) (count-group-constructor sl gsl 1))
+  (if (< count ga)
+      (cons (cons count (car sl)) (count-group-constructor (cdr sl) ga (+ count 1)))
+      (cons (cons count (car sl)) (count-group-constructor (cdr sl) ga 1))
       )
   )
 )
-;; Example group
-(define gc (group-counting student-list 5))
 
 ;; Balanced grouping by counting
+;; sl is student list
+;; ga is the desired amount of groups
+(define (group-count-balanced sl ga)
+  (let ([sex caddr]
+        [nationality cadddr])
+    ;; Sort first by sex, then by nationality to ensure a fair distribution when counting
+  (group-counting (sort (sort sl string<? #:key sex) string<? #:key nationality) ga))
+)
 
 ;; Random grouping with group predicate
 ;;; At least N students in a group are A years old or older
 ;;; All students in the group are female
 ;;; No students in the group are of the same age
+
+
+
+;; Example groups
+(define gc (group-counting student-list 6))
+(define gcb (group-count-balanced student-list 5))
+(define (stud)
+  car (car gc)
+)
